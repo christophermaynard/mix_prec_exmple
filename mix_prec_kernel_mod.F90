2b0130e164306e4de28e12487b338584e893f436
Christopher Maynard <c.m.maynard@reading.ac.uk>
2020-06-03 16:30:29 +0100

    Making code more like actual LFRic code.

--- mix_prec_kernel_mod.F90
+++ mix_prec_kernel_mod.F90
@@ -1,34 +1,44 @@
 module mix_prec_kernel_mod
+
+  use argument_mod, only : arg_type, GH_FIELD, GH_READ, GH_WRITE, ANY_SPACE_1, CELLS
   use, intrinsic :: iso_fortran_env, only : real32, real64, real128
+  use kernel_mod,   only : kernel_type
 
-  type, public :: mix_prec_kernel_type
+  type, public, extends(kernel_type) :: mix_prec_kernel_type
+     private
+     type(arg_type) :: meta_args(2) = (/                    &
+          arg_type( GH_FIELD, GH_WRITE, ANY_SPACE_1 ),      &
+          arg_type( GH_FIELD, GH_READ,  ANY_SPACE_1 )       &
+          /)
+     integer :: iterates_over = CELLS
    contains
+     procedure, nopass :: mix_prec_code
 
-     procedure, nopass :: sp => mix_prec_code_r32
-     procedure, nopass :: dp => mix_prec_code_r64
-     procedure, nopass :: qp => mix_prec_code_r128
-     generic, public :: mix_prec_code => sp, dp, qp
   end type mix_prec_kernel_type
 
+  interface mix_prec_code
+     module procedure mix_prec_code_r32, mix_prec_code_r64, mix_prec_code_r128
+  end interface mix_prec_code
+  
 contains
-  subroutine mix_prec_code_r32(val)
+  subroutine mix_prec_code_r32(f1, f2)
     implicit none
-    real(kind=real32), intent(in) :: val
-
+    real(kind=real32), intent(in) :: f1, f2
+    f1 = f2
     write(*,*) "32-bit"
   end subroutine mix_prec_code_r32
   
-  subroutine mix_prec_code_r64(val)
+  subroutine mix_prec_code_r64(f1, f2)
     implicit none
-    real(kind=real64), intent(in) :: val
-
+    real(kind=real64), intent(in) :: f1, f2
+    f1 = f2
     write(*,*) "64-bit"
   end subroutine mix_prec_code_r64
 
-  subroutine mix_prec_code_r128(val)
+  subroutine mix_prec_code_r128(f1, f2)
     implicit none
-    real(kind=real128), intent(in) :: val
-
+    real(kind=real128), intent(in) :: f1, f2
+    f1 = f2
     write(*,*) "128-bit"
   end subroutine mix_prec_code_r128
   
---
+++ mp_alg_mod.F90
@@ -0,0 +1,20 @@
+program mp_alg_mod
+
+
+  use mix_prec_kernel_mod, only : mix_prec_kernel_type
+  use field_mod, only : field_type, field_proxy_type
+  use rsolver_field_mod, only : rsolver_field_type, rsolver_field_proxy_type
+
+ type(field_type) :: f1
+ type(rsolver_field_type) :: f2
+
+ ! fill them with data
+ f1%data = 2.718_r_def
+ f2%data = 3.142_r_solver
+
+ ! call a kernel
+ call invoke( mix_prec_kernel_type( f1, f2 ) )
+
+ ! done!
+ 
+end program mp_alg_mod  
